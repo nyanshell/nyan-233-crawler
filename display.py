498d7b90cbdeb226aacf38ffc376eb3f732f26f5
gestapolur <gestapolur@gmail.com>
2013-06-07 00:21:16 +0800

    update frontend

--- display.py
+++ display.py
@@ -18,6 +18,7 @@ import webapp2
 import dbmodels
 import jinja2
 import os
+from dbmodels import zh_user, temp_user, dead_zh_user, Count
 from google.appengine.ext import db
 
 JINJA_ENVIRONMENT = jinja2.Environment(
@@ -25,9 +26,13 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
+        template_values = {
+            'zh_users_acitve': Count.user_active,
+            'zh_users_dead': count.user_dead,
+            'count_start_date': count_start_date,
+        }
 
-        #template = JINJA_ENVIRONMENT.get_template('index.html')
-        #self.response.write(template.render())
-        self.response.write("Nyan, it works.")
+        template = JINJA_ENVIRONMENT.get_template('show_tweet.html')
+        self.response.write(template.render(template_values))
     
 app = webapp2.WSGIApplication([('/', MainHandler)], debug=True)
